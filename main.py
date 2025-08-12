6932fd335e9565baab50fa6ce8baf109b3acbb63
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-08-12 13:28:40 -0700

    Do something chess-y

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from django-chess!")
+import chess
 
+def main():
+    board = chess.Board()
+    for move in ("e4"   ,
+                 "e5"   ,
+                 "Qh5"  ,
+                 "Nc6"  ,
+                 "Bc4"  ,
+                 "Nf6"  ,
+                 "Qxf7" ,
+                 ):
+        board.push_san(move)
+        print(f"{move}:")
+        print(board)
+        print()
 
 if __name__ == "__main__":
     main()
