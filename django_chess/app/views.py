89e1606e9cf33116cb298248cb7ccc5f91677105
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-08-13 17:57:22 -0700

    Add background color

--- django_chess/app/views.py
+++ django_chess/app/views.py
@@ -5,13 +5,14 @@ import chess.svg
 
 from django.shortcuts import get_object_or_404, render
 from django.utils.safestring import SafeString
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django_chess.app.models import Game
 
 # Create your views here.
+
 
 @require_http_methods(["POST"])
 def new_game(request: HttpRequest) -> HttpResponse:
@@ -20,24 +21,35 @@ def new_game(request: HttpRequest) -> HttpResponse:
     return HttpResponseRedirect(reverse("game", kwargs=dict(game_display_number=new_game.pk)))
 
 
-def get_pieces(board: chess.Board) -> Iterator[SafeString]:
-    for n in range(63, -1, -1):
-        piece = board.piece_map().get(n, None)
-        if piece is not None:
-            yield SafeString(chess.svg.piece(piece))
-        else:
-            yield SafeString("")
+def get_squares(board: chess.Board) -> Iterator[SafeString]:
+    for rank in range(7, -1, -1):
+        for file_ in range(8):
+            background_color_class = "light" if (rank - file_) % 2 else "dark"
+            n = chess.square(file_, rank)
+            piece = board.piece_map().get(n, None)
+
+            rv = {"class": background_color_class}
+
+            if piece is not None:
+                rv["piece"] = SafeString(chess.svg.piece(piece))
+            else:
+                rv["piece"] = SafeString("")
+
+            yield rv
+
 
 @require_http_methods(["GET"])
 def game(request: HttpRequest, game_display_number: int) -> HttpResponse:
     g = Game.objects.filter(pk=game_display_number).first()
+    if g is None:
+        return HttpResponseNotFound()
     board = chess.Board()
     board.set_board_fen(g.board_fen)
     return TemplateResponse(
         request,
         "app/game.html",
         context={
-            "board": str(board),
-            "pieces": get_pieces(board),
+            "outcome": str(board.outcome()),
+            "squares": get_squares(board),
         },
     )
