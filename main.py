066412cd56fd2bd07810c0c885a6bfee8a02c3bb
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-08-18 07:34:56 -0700

    Make the demo a tad more readable

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def main() -> None:
 
     with chess.engine.SimpleEngine.popen_uci([GNUCHESS_PATH, "--uci"]) as engine:
         while not board.is_game_over():
+            whose_turn = chess.COLOR_NAMES[board.turn]
             print()
-            print(f"{len(board.piece_map())} pieces")
+            print(f"{len(board.piece_map())} pieces; {whose_turn}'s turn")
             print(board)
 
             # Get engine move
-            result = engine.play(board, chess.engine.Limit(time=0.1))
-            print("Engine plays:", result.move)
+            result = engine.play(board, chess.engine.Limit(time=0 if board.turn else 1))
+            print(f"{whose_turn} plays:", result.move)
 
             # Apply engine move
             if result.move is not None:
@@ -26,6 +27,7 @@ def main() -> None:
             if board.is_game_over():
                 break
 
+        print(board)
         print("Game over:", board.result(), board.outcome())
 
 
