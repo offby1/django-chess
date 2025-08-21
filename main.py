f78f753fbf19bbacf3a19e67eafabb75b9be58cf
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-08-21 16:12:21 -0700

    Simplify demo

--- main.py
+++ main.py
@@ -7,35 +7,32 @@ import chess.engine
 def main() -> None:
     GNUCHESS_PATH = "gnuchess"
 
-    found_promotion = False
-
     with chess.engine.SimpleEngine.popen_uci([GNUCHESS_PATH, "--uci"]) as engine:
-        while not found_promotion:
-            board = chess.Board()
+        board = chess.Board()
 
-            while not board.is_game_over():
-                whose_turn = chess.COLOR_NAMES[board.turn]
-                print()
-                print(f"{len(board.piece_map())} pieces; {whose_turn}'s turn")
-                print(board)
-
-                # Get engine move
-                result = engine.play(board, chess.engine.Limit(time=0 if board.turn else 0))
-                print(f"{whose_turn} plays:", result.move)
-
-                # Apply engine move
-                if result.move is not None:
-                    board.push(result.move)
-                    if result.move.promotion is not None:
-                        print("HEY LOOK A PIECE GOT PROMOTED")
-                        found_promotion = True
-                        break
-
-                if board.is_game_over():
-                    break
-
+        while True:
+            whose_turn = chess.COLOR_NAMES[board.turn]
+            print()
+            print(f"{len(board.piece_map())} pieces; {whose_turn}'s turn")
             print(board)
-            print("Game over:", board.result(), board.outcome())
+
+            # Get engine move
+            result = engine.play(board, chess.engine.Limit(time=0 if board.turn else 0))
+            print(f"{whose_turn} plays:", result.move)
+
+            # Apply engine move
+            if result.move is not None:
+                if result.move.to_square == result.move.from_square:
+                    board.push(
+                        chess.Move.from_uci("0000")
+                    )  # I think this can happen in a stalemate
+                else:
+                    board.push(result.move)
+
+            if board.is_game_over():
+                print(board)
+                print("Game over:", board.result(), board.outcome())
+                break
 
 
 if __name__ == "__main__":
