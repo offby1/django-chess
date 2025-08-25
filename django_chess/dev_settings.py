32e0686fcc67717414a25858bae1268d91187372
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-08-24 19:17:04 -0700

    Crank up the logging, so we can see gnuchess think.

--- django_chess/dev_settings.py
+++ django_chess/dev_settings.py
@@ -1,5 +1,24 @@
+from typing import Any
+
 from django_chess.base_settings import *  # noqa
 from django_chess.base_settings import INSTALLED_APPS, MIDDLEWARE
 
 INSTALLED_APPS = ["debug_toolbar"] + INSTALLED_APPS
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+
+LOGGING: dict[str, Any] = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": [
+            "console",
+        ],
+        "level": "DEBUG",
+    },
+}
