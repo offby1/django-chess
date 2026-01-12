95e5bbc608bae1eaa86c85133180994b9e9ba540
Eric Hanchrow <eric.hanchrow@gmail.com>
2026-01-12 12:13:18 -0800

    Update ALLOWED_HOSTS
    I'm continually surprised that I cannot, apparently, predict which entries need to be in this :-\

--- django_chess/dev_settings.py
+++ django_chess/dev_settings.py
@@ -2,6 +2,14 @@ from typing import Any
 
 from django_chess.base_settings import *  # noqa
 
+ALLOWED_HOSTS.extend(
+    [
+        "127.0.0.1",
+        "10.0.2.2",  # android emulator
+    ]
+)
+
+
 LOGGING: dict[str, Any] = {
     "version": 1,
     "disable_existing_loggers": False,
